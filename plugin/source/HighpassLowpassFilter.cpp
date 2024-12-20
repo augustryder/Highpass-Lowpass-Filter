6ed9dd04660d58cda588330912a45b228ba7631d
August Herron <66635096+augustryder@users.noreply.github.com>
2024-12-20 13:03:16 -0600

    fixed arithmetic error, now working

--- plugin/source/HighpassLowpassFilter.cpp
+++ plugin/source/HighpassLowpassFilter.cpp
@@ -13,23 +13,24 @@ void HighpassLowpassFilter::setSamplingRate(float samplingRate) {
 }
 
 void HighpassLowpassFilter::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer&) {
-  constexpr auto PI = 3.1415927;
+  constexpr auto PI = 3.14159265359f;
   dnBuffer.resize(buffer.getNumChannels(), 0.f);
   const auto sign = highpass ? -1.f : 1.f;
-
   const auto tan = std::tan(PI * cutoffFrequency / samplingRate);
   const auto a1 = (tan - 1.f) / (tan + 1.f);
 
   for (auto channel = 0; channel < buffer.getNumChannels(); ++channel) {
+
     auto channelSamples = buffer.getWritePointer(channel);
-    
+
     for (auto i = 0; i < buffer.getNumSamples(); ++i) {
       const auto inputSample = channelSamples[i];
-      const auto allpassFilteredSample = a1 * inputSample * dnBuffer[channel];
+      const auto allpassFilteredSample = a1 * inputSample + dnBuffer[channel];
       dnBuffer[channel] = inputSample - a1 * allpassFilteredSample;
 
       const auto filterOutput = 0.5f * (inputSample + sign * allpassFilteredSample);
       channelSamples[i] = filterOutput;
     }
+
   }
 }
