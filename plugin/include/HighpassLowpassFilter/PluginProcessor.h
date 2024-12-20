2fd157af3d09cf80ac4da9ddacd278a52809a7db
August Herron <66635096+augustryder@users.noreply.github.com>
2024-12-19 23:47:13 -0600

    adding processor functionality

--- plugin/include/HighpassLowpassFilter/PluginProcessor.h
+++ plugin/include/HighpassLowpassFilter/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include "HighpassLowpassFilter.h"
 
 namespace audio_plugin {
 class AudioPluginAudioProcessor : public juce::AudioProcessor {
@@ -36,6 +37,10 @@ public:
   void setStateInformation(const void* data, int sizeInBytes) override;
 
 private:
+  juce::AudioProcessorValueTreeState parameters;
+  std::atomic<float>* cutoffFrequencyParameter = nullptr;
+  std::atomic<float>* highpassParameter = nullptr;
+  HighpassLowpassFilter filter;
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessor)
 };
 }  // namespace audio_plugin
--- plugin/source/PluginProcessor.cpp
+++ plugin/source/PluginProcessor.cpp
@@ -3,15 +3,22 @@
 
 namespace audio_plugin {
 AudioPluginAudioProcessor::AudioPluginAudioProcessor()
-    : AudioProcessor(
-          BusesProperties()
-#if !JucePlugin_IsMidiEffect
-#if !JucePlugin_IsSynth
-              .withInput("Input", juce::AudioChannelSet::stereo(), true)
-#endif
-              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
-#endif
-      ) {
+    : AudioProcessor(BusesProperties()
+                      #if !JucePlugin_IsMidiEffect
+                      #if !JucePlugin_IsSynth
+                        .withInput("Input", juce::AudioChannelSet::stereo(), true)
+                      #endif
+                        .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+                      #endif
+      ), 
+      parameters(*this, nullptr, juce::Identifier("HighpassLowpassPlugin"), 
+                 {std::make_unique<juce::AudioParameterFloat>("cutoff_frequency", "Cutoff Frequency", 
+                  juce::NormalisableRange{20.f, 20000.f, 0.1f, 0.2f, false}, 500.f), 
+                  std::make_unique<juce::AudioParameterBool>("highpass", "Highpass, false")}
+      )
+{
+  cutoffFrequencyParameter = parameters.getRawParameterValue("cutoff_frequency");
+  highpassParameter = parameters.getRawParameterValue("highpass");
 }
 
 AudioPluginAudioProcessor::~AudioPluginAudioProcessor() {}
@@ -76,7 +83,7 @@ void AudioPluginAudioProcessor::prepareToPlay(double sampleRate,
                                               int samplesPerBlock) {
   // Use this method as the place to do any pre-playback
   // initialisation that you need..
-  juce::ignoreUnused(sampleRate, samplesPerBlock);
+  filter.setSamplingRate(static_cast<float>(sampleRate));
 }
 
 void AudioPluginAudioProcessor::releaseResources() {
@@ -116,26 +123,17 @@ void AudioPluginAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
   auto totalNumInputChannels = getTotalNumInputChannels();
   auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-  // In case we have more outputs than inputs, this code clears any output
-  // channels that didn't contain input data, (because these aren't
-  // guaranteed to be empty - they may contain garbage).
-  // This is here to avoid people getting screaming feedback
-  // when they first compile a plugin, but obviously you don't need to keep
-  // this code if your algorithm always overwrites all the output channels.
   for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
     buffer.clear(i, 0, buffer.getNumSamples());
 
-  // This is the place where you'd normally do the guts of your plugin's
-  // audio processing...
-  // Make sure to reset the state if your inner loop is processing
-  // the samples and the outer loop is handling the channels.
-  // Alternatively, you can process the samples with the channels
-  // interleaved by keeping the same state.
-  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
-    auto* channelData = buffer.getWritePointer(channel);
-    juce::ignoreUnused(channelData);
-    // ..do something to the data...
-  }
+  const auto cutoffFrequency = cutoffFrequencyParameter->load();
+  const auto highpass = *highpassParameter < 0.5f ? false : true;
+
+  filter.setCutoffFrequency(cutoffFrequency);
+  filter.setHighpass(highpass);
+
+  filter.processBlock(buffer, midiMessages);
+
 }
 
 bool AudioPluginAudioProcessor::hasEditor() const {
@@ -143,7 +141,7 @@ bool AudioPluginAudioProcessor::hasEditor() const {
 }
 
 juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor() {
-  return new AudioPluginAudioProcessorEditor(*this);
+  return new AudioPluginAudioProcessorEditor(*this, parameters);
 }
 
 void AudioPluginAudioProcessor::getStateInformation(
