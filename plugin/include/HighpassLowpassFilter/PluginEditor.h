d89682f9c0f41cc6b6729f01af95c2b5e3d4abee
August Herron <66635096+augustryder@users.noreply.github.com>
2024-12-20 00:25:44 -0600

    added basic GUI

--- plugin/include/HighpassLowpassFilter/PluginEditor.h
+++ plugin/include/HighpassLowpassFilter/PluginEditor.h
@@ -6,7 +6,7 @@ namespace audio_plugin {
 
 class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor {
 public:
-  explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
+  explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&, juce::AudioProcessorValueTreeState& vts);
   ~AudioPluginAudioProcessorEditor() override;
 
   void paint(juce::Graphics&) override;
@@ -16,6 +16,14 @@ private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   AudioPluginAudioProcessor& processorRef;
+
+  juce::Slider cutoffFrequencySlider;
+  std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> cutoffFrequencyAttachment;
+  juce::Label cutoffFrequencyLabel;
+
+  juce::ToggleButton highpassButton;
+  std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> highpassAttachment;
+  juce::Label highpassButtonLabel;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -2,13 +2,31 @@
 #include "HighpassLowpassFilter/PluginProcessor.h"
 
 namespace audio_plugin {
-AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(
-    AudioPluginAudioProcessor& p)
-    : AudioProcessorEditor(&p), processorRef(p) {
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor& p, 
+                                                                 juce::AudioProcessorValueTreeState& vts)
+    : AudioProcessorEditor(&p), processorRef(p) 
+{
+  addAndMakeVisible(cutoffFrequencySlider);
+  cutoffFrequencySlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+  cutoffFrequencyAttachment.reset(new juce::AudioProcessorValueTreeState::SliderAttachment(
+    vts, "cutoff_frequency", cutoffFrequencySlider
+  ));
+
+  addAndMakeVisible(cutoffFrequencyLabel);
+  cutoffFrequencyLabel.setText("Cutoff Frequency", juce::dontSendNotification);
+
+  addAndMakeVisible(highpassButton);
+  highpassAttachment.reset(new juce::AudioProcessorValueTreeState::ButtonAttachment(
+    vts, "highpass", highpassButton
+  ));
+
+  addAndMakeVisible(highpassButtonLabel);
+  highpassButtonLabel.setText("Highpass", juce::dontSendNotification);
+
   juce::ignoreUnused(processorRef);
   // Make sure that before the constructor has finished, you've set the
   // editor's size to whatever you need it to be.
-  setSize(400, 300);
+  setSize(200, 400);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {}
@@ -16,17 +34,29 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {}
 void AudioPluginAudioProcessorEditor::paint(juce::Graphics& g) {
   // (Our component is opaque, so we must completely fill the background with a
   // solid colour)
-  g.fillAll(
-      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+  g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
   g.setColour(juce::Colours::white);
   g.setFont(15.0f);
-  g.drawFittedText("Hello World!", getLocalBounds(),
-                   juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized() {
   // This is generally where you'll want to lay out the positions of any
   // subcomponents in your editor..
+  const auto sliderX = cutoffFrequencySlider.getX();
+  const auto sliderY = cutoffFrequencySlider.getY();
+  const auto sliderHeight = cutoffFrequencySlider.getHeight();
+  const auto sliderWidth = cutoffFrequencySlider.getWidth();
+  const auto buttonX = highpassButton.getX();
+  const auto buttonY = highpassButton.getY();
+  const auto buttonHeight = highpassButton.getHeight();
+  const auto buttonWidth = highpassButton.getWidth();
+
+  cutoffFrequencySlider.setBounds({15, 35, 100, 300});
+  cutoffFrequencyLabel.setBounds({sliderX + 30, sliderY - 30, 200, 50});
+
+  highpassButton.setBounds({sliderX, sliderY + sliderHeight + 15, 30, 50});
+  highpassButtonLabel.setBounds({sliderX + buttonWidth + 15, buttonY, sliderWidth - buttonWidth, buttonHeight});
+              
 }
 }  // namespace audio_plugin
